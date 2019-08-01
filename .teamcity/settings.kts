b3f5080e02d1fea56b8f8cc1104cee5a6e8bc3c0
missingdays <missingdays@TeamCity>
2019-08-01 18:55:53 +0300

    TeamCity change in '<Root project>' project: Versioned settings configuration updated

---
+++ .teamcity/settings.kts
@@ -0,0 +1,79 @@
+import jetbrains.buildServer.configs.kotlin.v2018_2.*
+import jetbrains.buildServer.configs.kotlin.v2018_2.vcs.SvnVcsRoot
+
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
+
+version = "2019.1"
+
+project {
+    description = "Contains all other projects"
+
+    features {
+        feature {
+            id = "PROJECT_EXT_1"
+            type = "ReportTab"
+            param("startPage", "coverage.zip!index.html")
+            param("title", "Code Coverage")
+            param("type", "BuildReportTab")
+        }
+        feature {
+            id = "PROJECT_EXT_2"
+            type = "OAuthProvider"
+            param("clientId", "6b611d6e557a69ca5550")
+            param("defaultTokenScope", "public_repo,repo,repo:status,write:repo_hook")
+            param("secure:clientSecret", "credentialsJSON:0e1657af-6168-42bb-97f1-e51785b701aa")
+            param("displayName", "GitHub.com")
+            param("gitHubUrl", "https://github.com/")
+            param("providerType", "GitHub")
+        }
+    }
+
+    cleanup {
+        preventDependencyCleanup = false
+    }
+
+    subProject(Testing)
+}
+
+
+object Testing : Project({
+    name = "Testing"
+
+    vcsRoot(Testing_SpringPetclinic)
+
+    buildType(Testing_TestingKotlinDsl)
+})
+
+object Testing_TestingKotlinDsl : BuildType({
+    name = "Testing Kotlin DSL"
+
+    vcs {
+        root(Testing_SpringPetclinic)
+    }
+})
+
+object Testing_SpringPetclinic : SvnVcsRoot({
+    name = "SpringPetclinic"
+    url = "https://github.com/missingdays/spring-petclinic"
+    uploadedKey = "Github"
+})
