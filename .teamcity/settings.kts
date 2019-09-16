287049c7f1df007aa3c59027f741b94d25e517d2
missingdays <missingdays@TeamCity>
2019-09-16 10:29:05 +0300

    TeamCity change in 'ChatService' project: Versioned settings configuration updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,5 +1,7 @@
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
-import jetbrains.buildServer.configs.kotlin.v2018_2.vcs.SvnVcsRoot
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.maven
+import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.vcs
+import jetbrains.buildServer.configs.kotlin.v2018_2.vcs.GitVcsRoot
 
 /*
 The settings script is an entry point for defining a TeamCity
@@ -26,54 +28,41 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 version = "2019.1"
 
 project {
-    description = "Contains all other projects"
 
-    features {
-        feature {
-            id = "PROJECT_EXT_1"
-            type = "ReportTab"
-            param("startPage", "coverage.zip!index.html")
-            param("title", "Code Coverage")
-            param("type", "BuildReportTab")
-        }
-        feature {
-            id = "PROJECT_EXT_2"
-            type = "OAuthProvider"
-            param("clientId", "6b611d6e557a69ca5550")
-            param("defaultTokenScope", "public_repo,repo,repo:status,write:repo_hook")
-            param("secure:clientSecret", "credentialsJSON:0e1657af-6168-42bb-97f1-e51785b70682")
-            param("displayName", "GitHub.com")
-            param("gitHubUrl", "https://github.com/")
-            param("providerType", "GitHub")
-        }
-    }
+    vcsRoot(HttpsGithubComMissingdaysChatServiceRefsHeadsMaster)
 
-    cleanup {
-        preventDependencyCleanup = false
-    }
-
-    subProject(Testing)
+    buildType(Build)
 }
 
+object Build : BuildType({
+    name = "Build"
 
-object Testing : Project({
-    name = "Testing"
-
-    vcsRoot(Testing_SpringPetclinic)
-
-    buildType(Testing_TestingKotlinDsl)
-})
-
-object Testing_TestingKotlinDsl : BuildType({
-    name = "Testing Kotlin DSL"
+    params {
+        param("abc", "abc")
+    }
 
     vcs {
-        root(Testing_SpringPetclinic)
+        root(HttpsGithubComMissingdaysChatServiceRefsHeadsMaster)
+    }
+
+    steps {
+        maven {
+            goals = "clean test"
+            runnerArgs = "-Dmaven.test.failure.ignore=true"
+        }
+    }
+
+    triggers {
+        vcs {
+        }
     }
 })
 
-object Testing_SpringPetclinic : SvnVcsRoot({
-    name = "SpringPetclinic"
-    url = "https://github.com/missingdays/spring-petclinic"
-    uploadedKey = "Github"
+object HttpsGithubComMissingdaysChatServiceRefsHeadsMaster : GitVcsRoot({
+    name = "https://github.com/missingdays/ChatService#refs/heads/master"
+    url = "https://github.com/missingdays/ChatService"
+    authMethod = password {
+        userName = "missingdays"
+        password = "credentialsJSON:e232d6c4-6e94-42cd-84bd-ca619a4efbf1"
+    }
 })
